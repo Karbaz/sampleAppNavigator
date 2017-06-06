661efe180dfa9b417d43b1e28e3cf54935ee2768
arbaz.k@bewakoof.com <arbaz.k@bewakoof.com>
2017-06-06 15:59:32 +0530

    added back

--- app/tabOne/views/TabOneScreenOne.js
+++ app/tabOne/views/TabOneScreenOne.js
@@ -1,16 +1,30 @@
 'use strict'
 import React from 'react'
-import { View, Text, TouchableOpacity,Button } from 'react-native'
+import { View, Text, TouchableOpacity,Button,Alert } from 'react-native'
 export default class TabOneScreenOne extends React.Component {
 
    static navigationOptions = ({ navigation }) => ({
     title: 'Chat with',
-    // headerRight: (
-    //   <Button
-    //     title={'Done'}
+    headerRight: (
+      <Button
+        title={'Done'}
+        onPress={() => {Alert.alert('df')}}
+      />
+    ),
+    // header:(<Button
+    //     title={'Header'}
+    //     style={{backgroundColor:'lime'}}
     //     onPress={() => {Alert.alert('df')}}
-    //   />
-    // )
+    //   />)
+
+    headerTitle:'Titke',
+    headerBackTitle:null,
+
+    headerStyle:{
+      backgroundColor:'white'
+    },
+
+    gesturesEnabled:true
     });
 
 
